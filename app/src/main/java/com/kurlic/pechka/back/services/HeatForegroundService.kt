13dd7fe05810ae43981d9d6a93abdfef018d8a56
Alex Bogomol <quqveik.1@gmail.com>
2024-04-24 19:30:37 +0300

    Working template Foreground Service

--- app/src/main/java/com/kurlic/pechka/back/services/HeatForegroundService.kt
+++ app/src/main/java/com/kurlic/pechka/back/services/HeatForegroundService.kt
@@ -1,12 +1,52 @@
 package com.kurlic.pechka.back.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.os.HandlerThread
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
+import androidx.core.app.NotificationCompat
+import com.kurlic.pechka.MainActivity
+import com.kurlic.pechka.R
+import com.kurlic.pechka.common.debug.makeToast
 
 class HeatForegroundService : Service() {
+    private var serviceLooper: Looper? = null
+    private var serviceHandler: ServiceHandler? = null
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        HandlerThread("ServiceStartArguments").apply {
+            start()
+
+            // Get the HandlerThread's Looper and use it for our Handler
+            serviceLooper = looper
+            serviceHandler = ServiceHandler(looper)
+        }
+    }
+
+    private inner class ServiceHandler(looper: Looper) : Handler(looper) {
+
+        override fun handleMessage(msg: Message) {
+            try {
+                Thread.sleep(10000)
+            } catch (e: InterruptedException) {
+                Thread.currentThread().interrupt()
+            }
+
+            stopSelf(msg.arg1)
+        }
     }
 
     override fun onStartCommand(
@@ -14,10 +54,54 @@ class HeatForegroundService : Service() {
         flags: Int,
         startId: Int
     ): Int {
-        return super.onStartCommand(
-            intent,
-            flags,
-            startId
+        createNotificationChannel()
+        val notification = getNotification()
+        startForeground(
+            1,
+            notification
         )
+        serviceHandler?.obtainMessage()?.also { msg ->
+            msg.arg1 = startId
+            serviceHandler?.sendMessage(msg)
+        }
+
+        return START_NOT_STICKY
+    }
+
+    private val HeatServiceChannelID = "vbu"
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                HeatServiceChannelID,
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    private fun getNotification(): Notification {
+        val notificationIntent = Intent(
+            this,
+            MainActivity::class.java
+        )
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        return NotificationCompat.Builder(
+            this,
+            HeatServiceChannelID
+        )
+            .setContentTitle("Heat Service")
+            .setContentText("The service is running...")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentIntent(pendingIntent)
+            .build()
     }
 }
--- app/src/main/java/com/kurlic/pechka/ui/screens/HeatModeSelectScreen.kt
+++ app/src/main/java/com/kurlic/pechka/ui/screens/HeatModeSelectScreen.kt
@@ -1,15 +1,19 @@
 package com.kurlic.pechka.ui.screens
 
+import android.content.Intent
+import android.os.Build
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.kurlic.pechka.R
+import com.kurlic.pechka.back.services.HeatForegroundService
 import com.kurlic.pechka.ui.elements.StyledButton
 
 const val HeatModeSelectScreenName = "HeatModeSelectScreen"
@@ -17,12 +21,20 @@ const val HeatModeSelectScreenName = "HeatModeSelectScreen"
 @Composable
 @Preview
 fun HeatModeSelectScreen(navController: NavController = rememberNavController()) {
+    val context = LocalContext.current
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         StyledButton(
             text = stringResource(id = R.string.start_heating_service),
-            onClick = {})
+            onClick = {
+                val intent = Intent(context, HeatForegroundService::class.java)
+                if(Build.VERSION.SDK_INT >= 26) {
+                    context.startForegroundService(intent)
+                } else {
+                    context.startService(intent)
+                }
+            })
     }
 }
